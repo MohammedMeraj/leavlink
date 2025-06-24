910b6cac5eaaff1ff391072b1747c5ea5a332a3b
MohammedMeraj <mdmomin7517@gmail.com>
2025-06-25 01:17:18 +0530

    Test Static Files

--- src/app/f/page.tsx
+++ src/app/f/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function FileListPage() {
@@ -16,14 +15,14 @@ export default function FileListPage() {
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">All PDF Files</h1>
 
-      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <ul className="grid grid-cols-2  gap-8">
         {files.map((file, idx) => (
           <li key={idx}>
             <a
               href={`/files/${file}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
+              className="text-blue-600 hover:underline w-[80%]"
             >
               {file}
             </a>
@@ -31,9 +30,7 @@ export default function FileListPage() {
         ))}
       </ul>
 
-      <div className='w-full h-fit flex items-center justify-center'>
-        <Link className='text-lg ml-20 mt-10 font-bold underline text-gray-800' href="https://chatgpt.com/share/6859aef9-2268-8012-88ae-9e44c7830fc6">GPT Chat : Basic Formulae and EDA Steps</Link>
-        </div>
+      
     </div>
   );
 }
